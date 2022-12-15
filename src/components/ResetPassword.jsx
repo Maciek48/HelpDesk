60c5803d5d3a4dce77b72c410a3a50f5b83ed631
Maciej <maciejcieplowski@gmail.com>
2022-12-15 01:23:23 +0100

    zmiany w ResetPassword

--- src/components/ResetPassword.jsx
+++ src/components/ResetPassword.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useRef } from "react";
 import Input from "react-validation/build/input";
 import { useNavigate } from 'react-router-dom';
-import { Alert } from '@mui/material';
+import { Alert, CircularProgress } from '@mui/material';
 import Form from "react-validation/build/form";
 import UserService from "../services/userService";
 import CheckButton from "react-validation/build/button";
@@ -62,20 +62,33 @@ const ResetPassword = () => {
 
     return(
         <div>
-            <Form onSubmit={handleChangePassword}>
-                Reset Password
-                <div className="form-container">
-                    <div className="form-group">
-                        <label htmlFor="password">Password</label>
-                        <Input
-                        type="password"
-                        className="form-control"
-                        name="password"
-                        value={password}
-                        onChange={onChangePassword}
-                        validations={[required]}
-                        />
-                    </div>
+            
+              
+              <div className="form-container">
+                  Reset Password
+                <Form onSubmit={handleChangePassword}>
+                <div className="form-group">
+                  <label htmlFor="password">Password</label>
+                  <Input
+                    type="password"
+                    className="form-control"
+                    name="password"
+                    value={password}
+                    onChange={onChangePassword}
+                    validations={[required]}
+                  />
+                </div>
+              </div>
+
+                <div className="form-button-container">
+                  <button disabled={loading}>
+                    {loading && (
+                      <span><CircularProgress color="inherit"/></span>
+                    )}
+                    {!loading && (
+                      <span>Reset Password</span>
+                    )}
+                  </button>
                 </div>
 
                 {message && (
